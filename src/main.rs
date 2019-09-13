8d91b5b519c2dac1ba6745a6504968e093f223a1
Valentin Brandl <vbrandl@riseup.net>
2019-09-13 16:48:01 +0200

    Use `&str` over `String` where possible
    `String` is a heap allocated, mutable string. &str is immutable and does
    not require heap allocation. It is faster and more ergonomic to use.
    
    Also functions should take `&str` instead of `&String` as parameter
    since any `String` can be dereferenced as `&str`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::process;
-use std::string::String;
 
 mod lib;
 
 fn main() {
-    let version: String = String::from("2.0.2");
-    let architecture: String;
+    let version = "2.0.2";
+    let architecture: &str;
     let os_type: String;
 
     match lib::get_architecture() {
@@ -24,7 +23,7 @@ fn main() {
         }
     }
 
-    let filename = lib::generate_filename(os_type, architecture);
+    let filename = lib::generate_filename(&os_type, architecture);
     let base_path = lib::get_base_path();
     let tar_path = format!("{}/{}.tar.gz", base_path, filename);
     let binary_path = format!("{}/bin/{}", base_path, filename);
@@ -32,8 +31,8 @@ fn main() {
     if !lib::path_exists(&binary_path) {
         let base_url: String = lib::generate_base_url(version);
 
-        lib::download(base_url, &filename);
-        match lib::unpack(&tar_path, base_path) {
+        lib::download(&base_url, &filename);
+        match lib::unpack(&tar_path, &base_path) {
             Ok(()) => (),
             Err(err) => {
                 println!("{}", err);
